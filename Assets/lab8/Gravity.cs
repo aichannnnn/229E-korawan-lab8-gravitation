145aad1136027ada0bf7cc610a85bf0e4180e1fb
aichannnnn <139312651+aichannnnn@users.noreply.github.com>
2025-03-13 11:08:36 +0700

    finish

---
+++ Assets/lab8/Gravity.cs
@@ -0,0 +1,57 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour
+{
+    Rigidbody rb;
+    const float G = 0.00674f;
+    public static List<Gravity> gravityObjectList;
+
+    [SerializeField] bool planets = false;
+    [SerializeField] int orbetSpeed = 1000;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        if (gravityObjectList == null)
+        {
+            gravityObjectList = new List<Gravity>();
+        }
+
+        gravityObjectList.Add(this);
+
+
+        if (!planets)
+        {
+            rb.AddForce(Vector3.left * orbetSpeed);
+        }
+
+
+
+    }
+
+    private void FixedUpdate()
+    {
+        foreach (var obj in gravityObjectList)
+        {
+            if (obj != this)
+            {
+                Attract(obj);
+            }
+        }
+    }
+
+    void Attract(Gravity other)
+    {
+        Rigidbody otherRb = other.rb;
+
+        Vector3 direction = rb.position - otherRb.position;
+        float distance = direction.magnitude;
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+
+        otherRb.AddForce(finalForce);
+    }
+}
